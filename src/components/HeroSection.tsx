0c05f1a51216ff1bf7079c53c2f13f0a2a4fd9c5
Alexkid87IA <alex@nyac.fr>
2025-09-11 12:37:31 +0200

    fix: amélioration affichage mobile - titres plus grands et optimisations performances

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,9 +3,9 @@ import './HeroSection.css';
 
 export const HeroSection = () => {
   return (
-    <section className="hero-section">
-      {/* Rivière d'artistes en arrière-plan */}
-      <div className="artists-river">
+    <section className="hero-section" style={{ position: 'relative', height: '100vh', overflow: 'hidden' }}>
+      {/* Rivière d'artistes en arrière-plan - DANS LE HERO */}
+      <div className="artists-river" style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: 1 }}>
         {/* Rangée du haut */}
         <div className="artist-row top">
           <div className="artist-card"><img src="https://static.eno.do/x/fs-200359-default/9fb343deaad6dbe750cd731b4c0564b8/media.jpg" alt="Urbain" /></div>
@@ -26,6 +26,25 @@ export const HeroSection = () => {
           <div className="artist-card"><img src="https://static.eno.do/x/fs-200362-default/0743597244e1da871493bfbf5d13b7f7/media.jpg" alt="D'Jal" /></div>
           <div className="artist-card"><img src="https://i.imgur.com/munE7s3.jpeg" alt="Morgane Berling" /></div>
           <div className="artist-card"><img src="https://static.eno.do/x/fs-200364-default/2cf3c8b262adfc3c6e72e95639c39cf8/media.jpg" alt="Thomas Angelvy" /></div>
+        </div>
+
+        {/* Rangée du milieu - NOUVELLE */}
+        <div className="artist-row middle">
+          <div className="artist-card"><img src="https://26.staticbtf.eno.do/v1/29-default/caa1da7f867fc1ad334621eba4d80b76/media.jpg" alt="Julien Santini" /></div>
+          <div className="artist-card"><img src="https://static.eno.do/x/fs-200359-default/9fb343deaad6dbe750cd731b4c0564b8/media.jpg" alt="Urbain" /></div>
+          <div className="artist-card"><img src="https://static.eno.do/x/fs-200365-default/cda1d9f46d486a0ba2357daa5a79f6bd/media.jpg" alt="Lucie Carbone" /></div>
+          <div className="artist-card"><img src="https://i.imgur.com/ht3EucF.jpeg" alt="Sophie & Alex" /></div>
+          <div className="artist-card"><img src="https://static.eno.do/x/fs-200362-default/0743597244e1da871493bfbf5d13b7f7/media.jpg" alt="D'Jal" /></div>
+          <div className="artist-card"><img src="https://static.eno.do/x/fs-200360-default/a0c4d924ae52585a517dd76531300e5b/media.jpg" alt="Marc-Antoine Le Bret" /></div>
+          <div className="artist-card"><img src="https://26.staticbtf.eno.do/v1/30-default/975e3fdd1700df5c9bd53662949e3fda/media.jpg" alt="Djamel Comedy Club" /></div>
+          <div className="artist-card"><img src="https://26.staticbtf.eno.do/v1/29-default/caa1da7f867fc1ad334621eba4d80b76/media.jpg" alt="Julien Santini" /></div>
+          <div className="artist-card"><img src="https://static.eno.do/x/fs-200359-default/9fb343deaad6dbe750cd731b4c0564b8/media.jpg" alt="Urbain" /></div>
+          <div className="artist-card"><img src="https://static.eno.do/x/fs-200365-default/cda1d9f46d486a0ba2357daa5a79f6bd/media.jpg" alt="Lucie Carbone" /></div>
+          <div className="artist-card"><img src="https://i.imgur.com/ht3EucF.jpeg" alt="Sophie & Alex" /></div>
+          <div className="artist-card"><img src="https://static.eno.do/x/fs-200362-default/0743597244e1da871493bfbf5d13b7f7/media.jpg" alt="D'Jal" /></div>
+          <div className="artist-card"><img src="https://static.eno.do/x/fs-200360-default/a0c4d924ae52585a517dd76531300e5b/media.jpg" alt="Marc-Antoine Le Bret" /></div>
+          <div className="artist-card"><img src="https://26.staticbtf.eno.do/v1/30-default/975e3fdd1700df5c9bd53662949e3fda/media.jpg" alt="Djamel Comedy Club" /></div>
+          <div className="artist-card"><img src="https://26.staticbtf.eno.do/v1/29-default/caa1da7f867fc1ad334621eba4d80b76/media.jpg" alt="Julien Santini" /></div>
         </div>
 
         {/* Rangée du bas */}
@@ -62,9 +81,6 @@ export const HeroSection = () => {
         {/* Titres */}
         <h1 className="title-tiny">TINY TEAM,</h1>
         <h1 className="title-big">BIG DREAMS</h1>
-
-        {/* Sous-titre */}
-        <p className="subtitle">La scène vous attend</p>
 
         {/* Boutons */}
         <div className="buttons">
